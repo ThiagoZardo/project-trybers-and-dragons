e0251757dba180e08370c109668543ff496cfa1e
ThiagoZardo <thiago.artevisual@gmail.com>
2022-08-09 17:18:42 -0300

    Iniciando req 2

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,3 +1,13 @@
 import Race from './Race';
+import Dwarf from './Dwarf';
+import Elf from './Elf';
+import Halfing from './Halfling';
+import Orc from './Orc ';
 
 export default Race;
+export {
+  Dwarf,
+  Elf,
+  Halfing,
+  Orc,
+};
